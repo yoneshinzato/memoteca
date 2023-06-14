ea5a1c2cc5c66bee9f585e9639c83afb5790ad3d
yoneshinzato <yone.shinzato@montreal.com.br>
2023-06-13 22:39:20 -0300

    criar backend simulado com json-server e a interface de Pensamento

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento.interface';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,22 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [
-    {
-      conteudo: "Passo informações para o componente filho",
-      autoria: "Componente pai",
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: "Minha propriedade é decorada com @Input",
-      autoria: "Componente filho",
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, maxime? Excepturi deserunt in quia autem aliquam, nam magnam, temporibus doloremque qui quisquam natus et obcaecati impedit quod asperiores, sequi quos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, maxime? Excepturi deserunt in quia autem aliquam, nam magnam, temporibus doloremque qui quisquam natus et obcaecati impedit quod asperiores, sequi quos.",
-      autoria: "Componente filho",
-      modelo: 'modelo2'
-    },
-  ];
+  listaPensamentos: Pensamento[] = []
 
 }
