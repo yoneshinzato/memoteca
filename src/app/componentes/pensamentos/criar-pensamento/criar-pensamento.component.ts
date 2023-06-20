cf422105e59cb33ebca1e88e960e17364b7d177b
yoneshinzato <yone.shinzato@montreal.com.br>
2023-06-20 00:24:21 -0300

    utilizando validators required

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Pensamento } from './../pensamento.interface';
 import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -22,16 +22,19 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.fb.group({
-      conteudo: ['Formulário reativo'],
-      autoria: ['Angular'],
+      conteudo: ['', [Validators.required]],
+      autoria: ['', [Validators.required]],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(() =>
-    this.router.navigate(['/listarPensamento'])
-    );
+    console.log(this.formulario)
+    if(this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() =>
+      this.router.navigate(['/listarPensamento'])
+      );
+    }
 
   }
 
