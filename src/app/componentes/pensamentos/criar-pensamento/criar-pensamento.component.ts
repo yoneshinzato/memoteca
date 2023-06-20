3f31ee59b562509ccf57ba82619c0d67483c0c3a
yoneshinzato <yone.shinzato@montreal.com.br>
2023-06-20 10:49:25 -0300

    Validators pattern e minLength

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -22,14 +22,20 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.fb.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['', [Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    console.log(this.formulario)
+    console.log(this.formulario.status)
     if(this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() =>
       this.router.navigate(['/listarPensamento'])
